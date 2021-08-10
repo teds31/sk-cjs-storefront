a96acb025d1be58fc43a8ca407fb0f7e20e23a15
teds31 <tstanowski31@gmail.com>
2021-08-10 15:05:06 -0700

    switch from netlify to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify'
+import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
